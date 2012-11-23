5f478d006f86db729437f098705206113ec0246c
Jeremy Cook <jeremycook0@gmail.com>
2012-11-23 09:49:30 -0500

    Minor edits to config.

--- .bash
+++ .bash
@@ -1,6 +1,5 @@
 # Some bash customisations
-export TERM=xterm-256color
-#Tmux config
-[ -n "$TMUX" ] && export TERM=screen-256color
+export TERM=screen
+alias tmux="tmux -2"
 # Alias for composer. Allows command line apc to be enabled but assumes global installation of composer.
 alias composer="php -d apc.enable_cli=0 `which composer`"
