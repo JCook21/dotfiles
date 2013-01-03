c335afeb0378711ee485d2a0a6ab81cbcdc18bff
Jeremy Cook <jeremycook0@gmail.com>
2013-01-03 11:56:10 -0500

    Bash customisations to add smiley faces and git branch info.

--- .bash
+++ .bash
@@ -3,3 +3,9 @@ export TERM=screen
 alias tmux="tmux -2"
 # Alias for composer. Allows command line apc to be enabled but assumes global installation of composer.
 alias composer="php -d apc.enable_cli=0 `which composer`"
+# Add Git Branch info to the prompt
+parse_git_branch() {
+	git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
+}
+# Smiley faces and git branch info!
+PS1="\[\e[01;32m\]\u@\h \[\e[01;31m\]\$(parse_git_branch) \[\e[01;34m\]\w \`if [ \$? = 0 ]; then echo -e '\[\e[01;32m\]:)'; else echo -e '\[\e[01;31m\]:('; fi\` \[\e[01;34m\]$\[\e[00m\] "
