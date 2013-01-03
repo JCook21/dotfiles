885f66718c768afedb6a83a3aa1b2009004a5887
Jeremy Cook <jeremycook0@gmail.com>
2013-01-03 14:07:56 -0500

    Edits to make sure smiley face works when git branch is displayed.

--- .bash
+++ .bash
@@ -5,7 +5,7 @@ alias tmux="tmux -2"
 alias composer="php -d apc.enable_cli=0 `which composer`"
 # Add Git Branch info to the prompt
 parse_git_branch() {
-	git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
+	git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
 }
 # Smiley faces and git branch info!
-PS1="\[\e[01;36m\]\u@\h \[\e[01;31m\]\$(parse_git_branch) \[\e[01;34m\]\w \`if [ \$? = 0 ]; then echo -e '\[\e[01;32m\]:)'; else echo -e '\[\e[01;31m\]:('; fi\` \[\e[01;34m\]$\[\e[00m\] "
+PS1="\[\e[01;36m\]\u@\h \[\e[01;34m\]\w \`if [ \$? = 0 ]; then echo -e '\[\e[01;32m\]:)'; else echo -e '\[\e[01;31m\]:('; fi\` \[\e[01;35m\]\$(parse_git_branch) \[\e[01;34m\]$\[\e[00m\] "
