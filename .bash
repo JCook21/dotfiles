b8228533f402f730f628a612b77a3ea0f8ea03e4
Jeremy Cook <jeremycook0@gmail.com>
2013-01-03 12:36:05 -0500

    Change to colour.

--- .bash
+++ .bash
@@ -8,4 +8,4 @@ parse_git_branch() {
 	git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
 }
 # Smiley faces and git branch info!
-PS1="\[\e[01;32m\]\u@\h \[\e[01;31m\]\$(parse_git_branch) \[\e[01;34m\]\w \`if [ \$? = 0 ]; then echo -e '\[\e[01;32m\]:)'; else echo -e '\[\e[01;31m\]:('; fi\` \[\e[01;34m\]$\[\e[00m\] "
+PS1="\[\e[01;36m\]\u@\h \[\e[01;31m\]\$(parse_git_branch) \[\e[01;34m\]\w \`if [ \$? = 0 ]; then echo -e '\[\e[01;32m\]:)'; else echo -e '\[\e[01;31m\]:('; fi\` \[\e[01;34m\]$\[\e[00m\] "
