7c96a7ce9b6dd785c539d100a52ecb6bfec69cce
Jeremy Cook <jeremycook0@gmail.com>
2013-01-03 14:40:35 -0500

    Fix so that home and end keys work in Vim.

--- .bash
+++ .bash
@@ -1,5 +1,5 @@
 # Some bash customisations
-export TERM=screen
+export TERM=screen-256color
 alias tmux="tmux -2"
 # Alias for composer. Allows command line apc to be enabled but assumes global installation of composer.
 alias composer="php -d apc.enable_cli=0 `which composer`"
