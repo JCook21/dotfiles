31b3df3cccfca7143d15e822fc0913a8294294c8
Jeremy Cook <jeremycook0@icloud.com>
2019-01-21 09:20:43 -0500

    Updated files to fix alignment issue.

--- home/.guard.rb
+++ home/.guard.rb
@@ -1,7 +1,7 @@
 notification :terminal_notifier
 notification :tmux,
-  display_message: true,
-  timeout: 5,
-  default_message_format: '%s >> %s',
-  line_separator: ' > ',
-  color_location: 'status-left-bg'
+             display_message: true,
+             timeout: 5,
+             default_message_format: '%s >> %s',
+             line_separator: ' > ',
+             color_location: 'status-left-bg'
